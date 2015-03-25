bbdbd196c2550f6404314114d921bd92c9a11638
Vineeth Raj <contact.twn@openmailbox.org>
2015-03-26 02:59:36 +0530

    initial commit 26 mar 2k15

---
+++ app/src/main/java/teamcody/org/codycpanel/util/Helpers.java
@@ -0,0 +1,130 @@
+/*
+ * Performance Control - An Android CPU Control application Copyright (C) 2012
+ * Jared Rummler Copyright (C) 2012 James Roberts
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package teamcody.org.codycpanel.util;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class Helpers implements Constants {
+
+    /**
+     * Read one line from file
+     *
+     * @param fname
+     * @return line
+     */
+    public static String readOneLine(String fname) {
+        String line = null;
+        if (new File(fname).exists()) {
+            BufferedReader br;
+            try {
+                br = new BufferedReader(new FileReader(fname), 512);
+                try {
+                    line = br.readLine();
+                } finally {
+                    br.close();
+                }
+            } catch (Exception e) {
+                Log.e(TAG, "IO Exception when reading sys file", e);
+                // attempt to do magic!
+                return readFileViaShell(fname, true);
+            }
+        }
+        return line;
+    }
+
+    /**
+     * Read file via shell
+     *
+     * @param filePath
+     * @param useSu
+     * @return file output
+     */
+    public static String readFileViaShell(String filePath, boolean useSu) {
+        CMDProcessor.CommandResult cr = null;
+        if (useSu) {
+            cr = new CMDProcessor().su.runWaitFor("cat " + filePath);
+        } else {
+            cr = new CMDProcessor().sh.runWaitFor("cat " + filePath);
+        }
+        if (cr.success())
+            return cr.stdout;
+        return null;
+    }
+
+    /**
+     * Write one line to a file
+     *
+     * @param fname
+     * @param value
+     * @return if line was written
+     */
+    public static boolean writeOneLine(String fname, String value) {
+        if (!new File(fname).exists()) {
+            return false;
+        }
+
+        return writeFileViaShell(fname, value, true);
+
+        /*
+        try {
+            FileWriter fw = new FileWriter(fname);
+            try {
+                fw.write(value);
+            } finally {
+                fw.close();
+            }
+        } catch (IOException e) {
+            String Error = "Error writing to " + fname + ". Exception: ";
+            Log.e(TAG, Error, e);
+            return writeFileViaShell(fname, value, true);
+        }
+        return true;
+        */
+    }
+
+    /**
+     * Write file via shell
+     *
+     * @param filePath
+     * @param useSu
+     * @return file output
+     */
+    public static boolean writeFileViaShell(String filePath, String value, boolean useSu) {
+        CMDProcessor.CommandResult cr = null;
+        if (useSu) {
+            cr = new CMDProcessor().su.runWaitFor("echo " + value + " > " + filePath);
+        } else {
+            cr = new CMDProcessor().sh.runWaitFor("echo " + value + " > " + filePath);
+        }
+        if (cr.success())
+            return true;
+        return false;
+    }
+
+}
